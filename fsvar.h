0294e3e2906de29cafd1662956e9539d817d1dba
rsc <rsc>
2006-09-07 15:31:33 +0000

    comments

--- fsvar.h
+++ fsvar.h
@@ -1,11 +1,12 @@
 // in-core file system types
 
 struct inode {
-  uint dev;
-  uint inum;
-  int count;
-  int busy;
-  short type;
+  uint dev;           // Device number
+  uint inum;          // Inode number
+  int ref;            // Reference count
+  int busy;           // Is the inode "locked"?
+
+  short type;         // copy of disk inode
   short major;
   short minor;
   short nlink;
