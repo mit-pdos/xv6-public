e9ddc7c099b8f34a76295b60e0d93e033a9272b7
rsc <rsc>
2007-08-22 14:38:00 +0000

    NREQUEST = NBUF

--- param.h
+++ param.h
@@ -4,8 +4,8 @@
 #define NCPU          8  // maximum number of CPUs
 #define NOFILE       16  // open files per process
 #define NFILE       100  // open files per system
-#define NREQUEST    100  // outstanding disk requests
 #define NBUF         10  // size of disk block cache
+#define NREQUEST   NBUF  // outstanding disk requests
 #define NINODE      100  // maximum number of active i-nodes
 #define NDEV         10  // maximum major device number
 #define ROOTDEV       1  // device number of file system root disk
