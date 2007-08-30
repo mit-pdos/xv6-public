9863fea78cf88b40827d0d61ced2608278094ea4
rsc <rsc>
2007-08-30 18:30:26 +0000

    clumsy cd

--- sh.c
+++ sh.c
@@ -157,6 +157,14 @@ main(void)
   
   // Read and run input commands.
   while(getcmd(buf, sizeof(buf)) >= 0){
+    if(buf[0] == 'c' && buf[1] == 'd' && buf[2] == ' '){
+      // Clumsy but will have to do for now.
+      // Chdir has no effect on the parent if run in the child.
+      buf[strlen(buf)-1] = 0;  // chop \n
+      if(chdir(buf+3) < 0)
+        printf(2, "cannot cd %s\n", buf+3);
+      continue;
+    }
     if(fork1() == 0)
       runcmd(parsecmd(buf));
     wait();
