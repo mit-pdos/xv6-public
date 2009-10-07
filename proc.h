ab777a9ad0355e6df16ee53bad348d5fbb1f347f
Frans Kaashoek <kaashoek@30-7-101.wireless.csail.mit.edu>
2009-10-07 17:42:14 -0400

    fine-tune comment

--- proc.h
+++ proc.h
@@ -16,8 +16,8 @@
 // Contexts are stored at the bottom of the stack they
 // describe; the stack pointer is the address of the context.
 // The layout of the context matches the layout of the stack in swtch.S
-// at "Switch stacks" comment. eip is included  in the context 
-// so that allocproc() can easily change the return address on the stack.
+// at "Switch stacks" comment. Switch itself doesn't save eip explicitly,
+// but it is on the stack and allocproc() manipulates it.
 struct context {
   uint edi;
   uint esi;
