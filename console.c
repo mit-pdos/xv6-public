08eef9eae5989d3a709588352e228041e62e2c2c
Austin Clements <amdragon@mit.edu>
2010-09-02 18:50:49 -0400

    Get cgaputc and consputc back on the same page

--- console.c
+++ console.c
@@ -136,8 +136,7 @@ cgaputc(int c)
   if(c == '\n')
     pos += 80 - pos%80;
   else if(c == BACKSPACE){
-    if(pos > 0)
-      crt[--pos] = ' ' | 0x0700;
+    if(pos > 0) --pos;
   } else
     crt[pos++] = (c&0xff) | 0x0700;  // black on white
   
@@ -164,15 +163,12 @@ consputc(int c)
   }
 
   if(c == BACKSPACE){
-    uartputc('\b');
-    uartputc(' ');
-    uartputc('\b');
+    uartputc('\b'); uartputc(' '); uartputc('\b');
   } else
     uartputc(c);
   cgaputc(c);
 }
 
-//PAGEBREAK: 50
 #define INPUT_BUF 128
 struct {
   struct spinlock lock;
