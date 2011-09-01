d0f3efca650eccd5179e045cd07f7d723037defc
Austin Clements <amdragon@mit.edu>
2011-09-01 10:41:21 -0400

    Use stosl in memset; makes boot time bearable

--- string.c
+++ string.c
@@ -4,7 +4,11 @@
 void*
 memset(void *dst, int c, uint n)
 {
-  stosb(dst, c, n);
+  if ((int)dst%4 == 0 && n%4 == 0){
+    c &= 0xFF;
+    stosl(dst, (c<<24)|(c<<16)|(c<<8)|c, n/4);
+  } else
+    stosb(dst, c, n);
   return dst;
 }
 
