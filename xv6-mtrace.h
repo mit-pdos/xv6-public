54e63cc82e1aaf2ab43f658e67b2dda1460048bf
Silas Boyd-Wickizer <sbw@mit.edu>
2011-10-23 21:03:44 -0400

    An mtrace stub.

--- xv6-mtrace.h
+++ xv6-mtrace.h
@@ -19,4 +19,5 @@ char* strncpy(char *s, const char *t, int n);
 #include "mtrace-magic.h"
 #else
 #define mtrace_lock_register(ip, x, name, op, y)
+#define mtrace_label_register(t, r, x, y, z, ip)
 #endif
