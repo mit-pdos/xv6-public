95185df6c5f11495a0eaecbd05c9d4a7df96cf09
Silas Boyd-Wickizer <sbw@mit.edu>
2012-02-07 15:24:50 -0500

    Disable lockstat.
    You can enable by setting LOCKSTAT in param.h and use by running
    lockstat (e.g. $ lockstat mapbench).

--- param.h
+++ param.h
@@ -17,8 +17,8 @@
 #define VICTIMAGE 1000000 // cycles a proc executes before an eligible victim
 #define VERBOSE       0  // print kernel diagnostics
 #define	SPINLOCK_DEBUG 1 // Debug spin locks
-#define LOCKSTAT      1
-#define VERIFYFREE 1
+#define LOCKSTAT      0
+#define VERIFYFREE    LOCKSTAT
 #if defined(HW_josmp)
 #define NCPU         16  // maximum number of CPUs
 #define MTRACE       0
