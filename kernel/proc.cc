ba0c5af6ddc41e51b608a5773ecaba310bf9f867
Silas Boyd-Wickizer <sbw@mit.edu>
2012-03-15 21:07:57 -0400

    Move inituser to kernel/user.cc

--- kernel/proc.cc
+++ kernel/proc.cc
@@ -27,7 +27,7 @@ mycpuid(void)
 }
 
 xns<u32, proc*, proc_hash> *xnspid __mpalign__;
-static struct proc *bootproc __mpalign__;
+struct proc *bootproc __mpalign__;
 
 #if MTRACE
 struct kstack_tag kstack_tag[NCPU];
@@ -228,43 +228,6 @@ allocproc(void)
   p->context->rip = (uptr)forkret;
 
   return p;
-}
-
-// Set up first user process.
-void
-inituser(void)
-{
-  struct proc *p;
-  extern u8 _initcode_start[];
-  extern u64 _initcode_size;
-
-  p = allocproc();
-  p->ftable = new filetable();
-  if (p->ftable == nullptr)
-    panic("userinit: new filetable");
-  bootproc = p;
-  if((p->vmap = vmap::alloc()) == 0)
-    panic("userinit: out of vmaps?");
-  vmnode *vmn =  new vmnode(PGROUNDUP(_initcode_size) / PGSIZE);
-  if(vmn == 0)
-    panic("userinit: vmn_allocpg");
-  if(p->vmap->insert(vmn, 0, 1) < 0)
-    panic("userinit: vmap_insert");
-  if(p->vmap->copyout(0, _initcode_start, _initcode_size) < 0)
-    panic("userinit: copyout");
-  memset(p->tf, 0, sizeof(*p->tf));
-  p->tf->cs = UCSEG | 0x3;
-  p->tf->ds = UDSEG | 0x3;
-  p->tf->ss = p->tf->ds;
-  p->tf->rflags = FL_IF;
-  p->tf->rsp = PGSIZE;
-  p->tf->rip = 0x0;  // beginning of initcode.S
-
-  safestrcpy(p->name, "initcode", sizeof(p->name));
-  p->cwd = 0; // forkret will fix in the process's context
-  acquire(&p->lock);
-  addrun(p);
-  release(&p->lock);
 }
 
 void
