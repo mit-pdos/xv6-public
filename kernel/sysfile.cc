5232ea069adf66ac45a49ba3c1d4956512a7f1a0
Nickolai Zeldovich <nickolai@csail.mit.edu>
2012-03-24 17:18:28 -0400

    create() writes to abstract variable for inode

--- kernel/sysfile.cc
+++ kernel/sysfile.cc
@@ -215,6 +215,9 @@ create(inode *cwd, const char *path, short type, short major, short minor)
 {
   struct inode *ip, *dp;
   char name[DIRSIZ];
+  mt_ascope ascope("%s(%d.%d,%s,%d,%d,%d)",
+                   __func__, cwd->dev, cwd->inum,
+                   path, type, major, minor);
 
  retry:
   if((dp = nameiparent(cwd, path, name)) == 0)
@@ -238,6 +241,8 @@ create(inode *cwd, const char *path, short type, short major, short minor)
   ip->minor = minor;
   ip->nlink = 1;
   iupdate(ip);
+
+  mtwriteavar("inode:%x.%x", ip->dev, ip->inum);
 
   if(type == T_DIR){  // Create . and .. entries.
     dp->nlink++;  // for ".."
