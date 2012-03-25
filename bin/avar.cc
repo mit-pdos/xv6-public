aaf5a4a650c8b7638257f4c668907a63a52a83d7
Frans Kaashoek <kaashoek@mit.edu>
2012-03-24 23:23:52 -0400

    Shorter

--- bin/avar.cc
+++ bin/avar.cc
@@ -7,19 +7,10 @@
 int
 main(int ac, char **av)
 {
-  int op;
   if (ac == 2 && strcmp(av[1], "on") == 0)
-    op = 1;
-  else if (ac == 2 && strcmp(av[1], "off") == 0)
-    op = 0;
-  else {
-    fprintf(1, "usage: %s on|off\n", av[0]);
-    exit();
-  }
-
-  if (op) {
     mtenable_type(mtrace_record_ascope, "xv6-asharing");
-  } else {
+  else if (ac == 2 && strcmp(av[1], "off") == 0)
     mtdisable("xv6-asharing");  
-  }
+  else
+    fprintf(1, "usage: %s on|off\n", av[0]);
 }
