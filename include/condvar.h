380bfa4dcc0ee3a945e9c6e32255b9b862f8e6c2
Austin Clements <amdragon@mit.edu>
2012-04-27 15:43:27 -0400

    Make condvars constructable without initcondvar

--- include/condvar.h
+++ include/condvar.h
@@ -5,4 +5,13 @@
 struct condvar {
   struct spinlock lock;
   LIST_HEAD(waiters, proc) waiters;
+
+  // Create a condvar that will later be initialized by initcondvar.
+  // XXX(austin) Remove this and initcondvar.
+  constexpr condvar()
+    : lock(), waiters{} { }
+
+  constexpr
+  condvar(const char *name)
+    : lock(name, LOCKSTAT_CONDVAR), waiters{} { }
 };
