6fc6a394e3cc64bd27052d480ff2baf11354af54
Peter H. Froehlich <peter.hans.froehlich@gmail.com>
2015-10-18 00:40:12 -0400

    More sanity checks, slightly improved header.

--- mp.c
+++ mp.c
@@ -45,7 +45,7 @@ mpsearch1(uint a, int len)
 // spec is in one of the following three locations:
 // 1) in the first KB of the EBDA;
 // 2) in the last KB of system base memory;
-// 3) in the BIOS ROM between 0xE0000 and 0xFFFFF.
+// 3) in the BIOS ROM between 0xF0000 and 0xFFFFF.
 static struct mp*
 mpsearch(void)
 {
@@ -81,7 +81,7 @@ mpconfig(struct mp **pmp)
   conf = (struct mpconf*) P2V((uint) mp->physaddr);
   if(memcmp(conf, "PCMP", 4) != 0)
     return 0;
-  if(conf->version != 1 && conf->version != 4)
+  if(conf->specrev != 1 && conf->specrev != 4)
     return 0;
   if(sum((uchar*)conf, conf->length) != 0)
     return 0;
@@ -106,7 +106,17 @@ mpinit(void)
     switch(*p){
     case MPPROC:
       proc = (struct mpproc*)p;
-      if (ncpu >= NCPU) {
+      if(!(proc->flags & MP_PROC_ENABLED)){
+        cprintf("mpinit: ignoring cpu %d, not enabled\n", proc->apicid);
+        p += sizeof(struct mpproc);
+        continue;
+      }
+      if(!(proc->feature & MP_PROC_APIC)){
+        cprintf("mpinit: cpu %d has no working APIC, ignored\n", proc->apicid);
+        p += sizeof(struct mpproc);
+        continue;
+      }
+      if(ncpu >= NCPU){
         cprintf("mpinit: more than %d cpus, ignoring cpu%d\n",
           NCPU, proc->apicid);
         p += sizeof(struct mpproc);
@@ -122,7 +132,12 @@ mpinit(void)
       continue;
     case MPIOAPIC:
       ioapic = (struct mpioapic*)p;
-      ioapicid = ioapic->apicno;
+      if(!(ioapic->flags & MP_APIC_ENABLED)){
+        cprintf("mpinit: ioapic %d disabled, ignored\n", ioapic->apicid);
+        p += sizeof(struct mpioapic);
+        continue;
+      }
+      ioapicid = ioapic->apicid;
       p += sizeof(struct mpioapic);
       continue;
     case MPBUS:
