2a675089b129adf442de575617a7e3db5a42a794
MikeCAT <mikecat@users.noreply.github.com>
2015-12-19 22:52:11 +0900

    fix memory region validation in argptr()

--- syscall.c
+++ syscall.c
@@ -58,7 +58,7 @@ argptr(int n, char **pp, int size)
 
   if(argint(n, &i) < 0)
     return -1;
-  if((uint)i >= proc->sz || (uint)i+size > proc->sz)
+  if(size < 0 || (uint)i >= proc->sz || (uint)i+size > proc->sz)
     return -1;
   *pp = (char*)i;
   return 0;
