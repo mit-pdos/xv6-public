3f8b79881c726884283f84c9fe26bde5fe081326
TheMatrixShibe <thematrixshibe@example.com>
2016-02-09 20:17:50 +1100

    Fixed stuff!

--- param.h
+++ param.h
@@ -1,14 +1,14 @@
 #define NPROC        64  // maximum number of processes
-#define KSTACKSIZE 1024  // size of per-process kernel stack
+#define KSTACKSIZE 4096  // size of per-process kernel stack
 #define NCPU          8  // maximum number of CPUs
-#define NOFILE       64  // open files per process
-#define NFILE      4096  // open files per system
-#define NINODE      100  // maximum number of active i-nodes
+#define NOFILE       16  // open files per process
+#define NFILE       100  // open files per system
+#define NINODE       50  // maximum number of active i-nodes
 #define NDEV         10  // maximum major device number
 #define ROOTDEV       1  // device number of file system root disk
 #define MAXARG       32  // max exec arguments
-#define MAXOPBLOCKS 128  // max # of blocks any FS op writes
-#define LOGSIZE      48  // max data blocks in on-disk log
-#define NBUF         64  // size of disk block cache
-#define FSSIZE    16384  // size of file system in blocks
+#define MAXOPBLOCKS  10  // max # of blocks any FS op writes
+#define LOGSIZE      30  // max data blocks in on-disk log
+#define NBUF         30  // size of disk block cache
+#define FSSIZE    10000  // size of file system in blocks
 
