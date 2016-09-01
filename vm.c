4b2152cc2d46865f3e4be011698b7a49bca794b8
Robert Morris <rtm@csail.mit.edu>
2016-09-01 17:21:37 -0400

    curproc -> proc in a comment

--- vm.c
+++ vm.c
@@ -27,7 +27,7 @@ seginit(void)
   c->gdt[SEG_UCODE] = SEG(STA_X|STA_R, 0, 0xffffffff, DPL_USER);
   c->gdt[SEG_UDATA] = SEG(STA_W, 0, 0xffffffff, DPL_USER);
 
-  // Map cpu and curproc -- these are private per cpu.
+  // Map cpu and proc -- these are private per cpu.
   c->gdt[SEG_KCPU] = SEG(STA_W, &c->cpu, 8, 0);
 
   lgdt(c->gdt, sizeof(c->gdt));
