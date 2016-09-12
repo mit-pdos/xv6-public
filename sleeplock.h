564a1cf0cb867f7738fcbe466aec02081a16d3ab
Frans Kaashoek <kaashoek@mit.edu>
2016-09-11 21:01:24 -0400

    sleeplock files

---
+++ sleeplock.h
@@ -0,0 +1,10 @@
+// Long-term locks for processes
+struct sleeplock {
+  uint locked;       // Is the lock held?
+  struct spinlock lk; // spinlock protecting this sleep lock
+  
+  // For debugging:
+  char *name;        // Name of lock.
+  int pid;           // Process holding lock
+};
+
