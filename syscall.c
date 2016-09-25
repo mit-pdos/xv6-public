0df5f11baef529f32d81bb1b1580264067477a5e
Frans Kaashoek <kaashoek@mit.edu>
2016-09-25 08:27:26 -0400

    Typo (Thanks to Austin Liew)

--- syscall.c
+++ syscall.c
@@ -49,7 +49,7 @@ argint(int n, int *ip)
 }
 
 // Fetch the nth word-sized system call argument as a pointer
-// to a block of memory of size n bytes.  Check that the pointer
+// to a block of memory of size bytes.  Check that the pointer
 // lies within the process address space.
 int
 argptr(int n, char **pp, int size)
