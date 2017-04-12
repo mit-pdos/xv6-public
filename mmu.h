960e88651bf614d799ff19db8e77a5f4842e57ab
elazar125 <elazar125@gmail.com>
2017-04-12 17:06:44 -0500

    Add paged out flag

--- mmu.h
+++ mmu.h
@@ -143,6 +143,7 @@ struct segdesc {
 #define PTE_D           0x040   // Dirty
 #define PTE_PS          0x080   // Page Size
 #define PTE_MBZ         0x180   // Bits must be zero
+#define PTE_PG          0x200   // Paged out of memory into file
 
 // Address in page table or page directory entry
 #define PTE_ADDR(pte)   ((uint)(pte) & ~0xFFF)
