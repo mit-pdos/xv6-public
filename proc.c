bbdb8741014a6863b66d4dcfd20c841da786a3ef
Jakob Eriksson <jakob@uic.edu>
2017-10-20 22:39:09 -0500

    dropped some debug output

--- proc.c
+++ proc.c
@@ -362,7 +362,6 @@ forkret(void)
   static int first = 1;
   // Still holding ptable.lock from scheduler.
   release(&ptable.lock);
-  cprintf("forkret\n");
 
   if (first) {
     // Some initialization functions must be run in the context
