0f90388c893d1924e89e2e4d2187eda0004e9d73
Frans Kaashoek <kaashoek@mit.edu>
2018-10-10 21:50:58 -0400

    No T_SYSCALL

--- traps.h
+++ traps.h
@@ -22,9 +22,6 @@
 #define T_MCHK          18      // machine check
 #define T_SIMDERR       19      // SIMD floating point error
 
-// These are arbitrarily chosen, but with care not to overlap
-// processor defined exceptions or interrupt vectors.
-#define T_SYSCALL       64      // system call
 #define T_DEFAULT      500      // catchall
 
 #define T_IRQ0          32      // IRQ 0 corresponds to int T_IRQ
