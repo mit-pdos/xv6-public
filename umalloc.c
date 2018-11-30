7c011e2033721ffadcb8416d7f7448b10ba32e99
za10204iii <za10204iii@gmail.com>
2018-11-30 22:24:17 +0800

    urealloc bug fix

--- umalloc.c
+++ umalloc.c
@@ -108,8 +108,8 @@ urealloc(void* curp, uint nbytes)
         return 0;
       memmove(newp, curp, chp->s.size);
       free(curp);
-      printf(1,"curp=%p, csize=%d, nbytes=%d \n", curp, chp->s.size);
-      printf(1,"newp=%p, nsize=%d, nbytes=%d \n", newp, newp->s.size);
+      printf(1,"curp=%p, csize=%d \n", curp, chp->s.size);
+      printf(1,"newp=%p, nsize=%d \n", newp, newp->s.size);
     } else {
       printf(1,"exist! use curr.\t curp=%p, csize=%d, nbytes=%d \n", curp, chp->s.size, nbytes);
       newp = curp;
