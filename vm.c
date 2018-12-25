9bf33676afd679d528b521a8893484f197691ba0
za10204iii <za10204iii@gmail.com>
2018-12-25 21:21:39 +0800

    fix bug

--- vm.c
+++ vm.c
@@ -342,7 +342,7 @@ copyuvm(pde_t *pgdir, uint sz)
     if((mem = kalloc()) == 0)
       goto bad;
     memmove(mem, (char*)P2V(pa), PGSIZE);
-    if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0) {
+    if(mappages(pgpdir, (void*)i, PGSIZE, V2P(mem), flags) < 0) {
       kfree(mem);
       goto bad;
     }
