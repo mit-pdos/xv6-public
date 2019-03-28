fe5c609e9bf29adb13b690ad6bdb9b3b3a442c1e
DerickHass <dhasseldieck@alumnos.uai.cl>
2019-03-28 19:30:02 -0300

    Take 21
    Why am I still here

--- cuenta.c
+++ cuenta.c
@@ -1,9 +1,10 @@
 #include "types.h"
 #include "stat.h"
 #include "stdio.h"
+#include "defs.h"
 
 
 int main(void) {
-	printf(1,"La cantidad de procesos en ejecucion en la CPU es %i\n", getprocs());
+	printf("La cantidad de procesos en ejecucion en la CPU es %i\n", getprocs());
 	exit();
 }
