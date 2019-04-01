0b6c58782b6177efc83b3ffd03006f7b535c6f63
Diego Ramos <diegor98@gmail.com>
2019-04-01 17:59:01 -0300

    s

--- defs.h
+++ defs.h
@@ -120,6 +120,7 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
+int             getprocs(void);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
