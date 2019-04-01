36a1d77c2bf3e97b99ffb49e1ed3fceb83bf4434
Carlos <caleniz@alumnos.uai.cl>
2019-04-01 17:10:25 -0300

    Initial travis release

--- cuenta.c
+++ cuenta.c
@@ -4,6 +4,7 @@
 
 
 int main(void) {
-printf(1,"La cantidad de procesos en ejecucion en la CPU es %i\n", getprocs());
+printf(1,"La cantidad de procesos en ejecucion en la CPU es %d\n", getprocs());
+exit();
 }
  
