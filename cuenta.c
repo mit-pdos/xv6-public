53cf6d3d4dd79ba6bbeb3d5eba894a2ea8e840ed
DerickHass <dhasseldieck@alumnos.uai.cl>
2019-04-02 20:03:41 -0300

    Update cuenta.c

--- cuenta.c
+++ cuenta.c
@@ -4,6 +4,6 @@
 #include "user.h"
 
 int main(void) {
-	printf("La cantidad de procesos en ejecucion en la CPU es %i\n", getprocs());
+	printf(1,"La cantidad de procesos en ejecucion en la CPU es %i\n", getprocs());
 	return 0;
 }
