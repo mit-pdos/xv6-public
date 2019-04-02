c28874e0ce5575184cbb70ccfd72c40f7f625799
Diego Ramos <diegor98@gmail.com>
2019-04-02 09:42:55 -0300

    idiotes

--- cuenta.c
+++ cuenta.c
@@ -4,6 +4,6 @@
 #include "fcntl.h"
 
 int main(void) {
-    printf(1, "La cantidad de procesos en ejecucion en la CPU es %i\n", getprocs());
+    printf(1, "La cantidad de procesos en ejecucion en la CPU es %d\n", getprocs());
     exit();
 }
