81a012957c1488f4e836e4bfb34423fbccfa76bd
Diego Ramos <diegor98@gmail.com>
2019-04-02 08:53:33 -0300

    Correccion logica y nuevas librerias en cuenta.c

--- cuenta.c
+++ cuenta.c
@@ -1,5 +1,8 @@
-#include <stdio.h>
+#include <types.h>
+#include <user.h>
+#include <stat.h>
+
 
 int main(void) {
-    printf("La cantidad de procesos en ejecucion en la CPU es %i\n", getprocs());
+    printf(1, "La cantidad de procesos en ejecucion en la CPU es %i\n", getprocs());
 }
