f3f4d248df4363522f9a3a93f292ada237b0d619
Diego Ramos <diegor98@gmail.com>
2019-04-02 09:19:41 -0300

    correction in makefile

--- cuenta.c
+++ cuenta.c
@@ -1,7 +1,7 @@
 #include <types.h>
 #include <user.h>
 #include <stat.h>
-
+#include <fcntl.h>
 
 int main(void) {
     printf(1, "La cantidad de procesos en ejecucion en la CPU es %i\n", getprocs());
