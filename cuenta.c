07536a1fac58a6f4e3b0c0ad58624a81ef83c7a7
DerickHass <dhasseldieck@alumnos.uai.cl>
2019-04-02 19:53:48 -0300

    Update cuenta.c

--- cuenta.c
+++ cuenta.c
@@ -1,7 +1,8 @@
 #include "types.h"
 #include "stat.h"
 #include "defs.h"
-#include <stido.h>
+#include "user.h"
+
 int main(void) {
 	printf("La cantidad de procesos en ejecucion en la CPU es %i\n", getprocs());
 	return 0;
