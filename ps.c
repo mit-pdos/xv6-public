d634b722dd220503885530131c2fa3f8b5836496
MaxJEllis <max.ellis@wsu.edu>
2019-04-15 19:27:38 -0700

    Implemented sorting by size

--- ps.c
+++ ps.c
@@ -2,7 +2,7 @@
 #include "user.h"
 #include "uproc.h"
 
-void sort_table(struct uproc *table, int procnum)
+void sort_size(struct uproc *table, int procnum)
 {
   int i, j;
   struct uproc key;
@@ -10,11 +10,11 @@ void sort_table(struct uproc *table, int procnum)
   for (i = 1; i < procnum; i++) {
     key = table[i];
     j = i-1;
-
-    while (j >= 0 && table[i].state > key.state) {
+    while ((j >= 0) && (table[j].sz > key.sz)) {
       table[j+1] = table[j];
       j--;
     }
+    table[j+1] = key;
   }
 
 }
@@ -43,15 +43,18 @@ main(int argc, char* argv[])
       printf(1, "Found sp option\n");
       //sort_state(table, procnum);
     } 
+    // Print number of processes
     else if (strcmp(argv[1], "-n") == 0){
       printf(1, "Number of processes: %d\n", procnum);
     }
-    
+    // Sort by process size
+    else if (strcmp(argv[1], "-ss") == 0){
+      sort_size(table, procnum);
+    } 
     // Search by process name
     else if (strcmp(argv[1], "-s") == 0) {
     }
   }
-
 
   printf(1, "PID  PPID  STATE TIME UPTIME  SIZE  NAME\n");
   for(int i = 0; i < procnum; i++) // Print information for each process
@@ -75,6 +78,7 @@ main(int argc, char* argv[])
     printf(1, "%d  ", table[i].sz);
     printf(1, "%s\n", table[i].name);
   }
+
   free(table);
   exit();
 }
