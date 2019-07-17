ce53416f4970ebb137c8e66dc75488cfefaf084d
Frans Kaashoek <kaashoek@mit.edu>
2019-07-17 05:53:34 -0400

    Delete x86 comment

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -7,12 +7,6 @@
 #include "syscall.h"
 #include "defs.h"
 
-// User code makes a system call with INT T_SYSCALL.
-// System call number in %eax.
-// Arguments on the stack, from the user call to the C
-// library system call function. The saved user %esp points
-// to a saved program counter, and then the first argument.
-
 // Fetch the int at addr from the current process.
 int
 fetchint(uint64 addr, int *ip)
