fb8a0099d48643775d0bca626af1a73a3ab618a4
Robert Morris <rtm@csail.mit.edu>
2019-08-01 14:59:20 -0400

    fences for starting

--- kernel/main.c
+++ kernel/main.c
@@ -26,10 +26,12 @@ main()
     fileinit();      // file table
     virtio_disk_init(); // emulated hard disk
     userinit();      // first user process
+    __sync_synchronize();
     started = 1;
   } else {
     while(started == 0)
       ;
+    __sync_synchronize();
     printf("hart %d starting\n", cpuid());
     kvminithart();    // turn on paging
     trapinithart();   // install kernel trap vector
