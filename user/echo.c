c0beeccb1b46cfba7762740b901ca266adb65a6f
Robert Morris <rtm@csail.mit.edu>
2019-09-04 10:12:53 -0400

    have echo use FD 1 for clarity

--- user/echo.c
+++ user/echo.c
@@ -7,7 +7,13 @@ main(int argc, char *argv[])
 {
   int i;
 
-  for(i = 1; i < argc; i++)
-    printf("%s%s", argv[i], i+1 < argc ? " " : "\n");
+  for(i = 1; i < argc; i++){
+    write(1, argv[i], strlen(argv[i]));
+    if(i + 1 < argc){
+      write(1, " ", 1);
+    } else {
+      write(1, "\n", 1);
+    }
+  }
   exit();
 }
