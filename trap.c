0eed5b31b96c33cb5109def80627e94ab690c7c3
B V K <57064909+khadiravana-belagavi@users.noreply.github.com>
2019-11-03 13:07:19 +0530

    Update trap.c

--- trap.c
+++ trap.c
@@ -58,12 +58,12 @@ trap(struct trapframe *tf)
         if(myproc()->state == RUNNING)
         {
           myproc()->running_time++;
-          cprintf("\nUpdated rtime %d\n",myproc()->running_time);
+//           cprintf("\nUpdated rtime %d\n",myproc()->running_time);
         }
         else if(myproc()->state == SLEEPING)
         {
           myproc()->io_time++;
-          cprintf("\nUpdated iotime: %d\n",myproc()->io_time);
+//           cprintf("\nUpdated iotime: %d\n",myproc()->io_time);
         }
      
       }
