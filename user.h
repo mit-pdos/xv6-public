6ec805d914f003a943845243d5a80afe39e61255
B V K <57064909+khadiravana-belagavi@users.noreply.github.com>
2019-11-03 06:45:33 +0530

    Added waitx

--- user.h
+++ user.h
@@ -23,7 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
-
+int waitx(int *wtime, int *rtime);
 // ulib.c
 int stat(const char*, struct stat*);
 char* strcpy(char*, const char*);
