42bb1bb0dc984ed81fb49f07a9032dead2483ae3
B V K <57064909+khadiravana-belagavi@users.noreply.github.com>
2019-11-03 11:22:32 +0530

    Update trap.c

--- trap.c
+++ trap.c
@@ -51,14 +51,14 @@ trap(struct trapframe *tf)
     if(cpuid() == 0){
       acquire(&tickslock);
       ticks++;
-      wakeup(&ticks);
-      release(&tickslock);
   //BVK Assignment 5 Addition for waitx    
       if(myproc()){
         if(myproc()->state == RUNNING)
           myproc()->running_time++;
         else if(myproc()->state == SLEEPING)
           myproc()->io_time++;
+      wakeup(&ticks);
+      release(&tickslock);
       }
       // End
     }  
