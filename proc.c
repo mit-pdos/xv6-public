a78ac1d6b3016131089228091b5da710a2181a73
B V K <57064909+khadiravana-belagavi@users.noreply.github.com>
2019-11-03 06:17:42 +0530

    Proc.c
    Added Time variables.

--- proc.c
+++ proc.c
@@ -111,7 +111,16 @@ found:
   p->context = (struct context*)sp;
   memset(p->context, 0, sizeof *p->context);
   p->context->eip = (uint)forkret;
-
+  
+  //BVK - Assignmment 5 Addition:
+  
+  //Initializing 
+  
+  fields;
+  p->start_time = ticks;
+  p->end_time = 0;
+  p->io_time = 0;
+  p->running_time = 0;
   return p;
 }
 
@@ -263,6 +272,8 @@ exit(void)
 
   // Jump into the scheduler, never to return.
   curproc->state = ZOMBIE;
+  //BVK Assignment 5 Q1 Commit.
+  proc->end_time = ticks;     //Updating the end time at exit.
   sched();
   panic("zombie exit");
 }
@@ -310,6 +321,52 @@ wait(void)
     sleep(curproc, &ptable.lock);  //DOC: wait-sleep
   }
 }
+
+int
+waitx(int *wait_time , int *rtime)
+{
+  struct proc *p;
+  int havekids, pid;
+  struct proc *curproc = myproc();
+  
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for exited children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->parent != curproc)
+        continue;
+      havekids = 1;
+      if(p->state == ZOMBIE){
+        // Found one.
+        // Update the Time Variables:
+        *wait_time = p->end_time - p->start_time - p->running_time - p->io_time;time
+        
+        pid = p->pid;
+        kfree(p->kstack);
+        p->kstack = 0;
+        freevm(p->pgdir);
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->state = UNUSED;
+        release(&ptable.lock);
+        return pid;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || curproc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
+  }
+}
+
 
 //PAGEBREAK: 42
 // Per-CPU process scheduler.
