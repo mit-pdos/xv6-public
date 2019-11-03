e34e627d266b1b8ebe18d1c5adbad66c809c1c2f
B V K <57064909+khadiravana-belagavi@users.noreply.github.com>
2019-11-03 06:53:07 +0530

    Update defs.h

--- defs.h
+++ defs.h
@@ -118,6 +118,7 @@ void            setproc(struct proc*);
 void            sleep(void*, struct spinlock*);
 void            userinit(void);
 int             wait(void);
+int             waitx(int *,int *);   //BVK addition Assignment 5 Q1
 void            wakeup(void*);
 void            yield(void);
 
