83d88862dd9be7434b094fe42f2034f26faed944
B V K <57064909+khadiravana-belagavi@users.noreply.github.com>
2019-11-04 06:10:11 +0530

    Update time.c

--- time.c
+++ time.c
@@ -1,26 +1,33 @@
+#include "param.h"
 #include "types.h"
 #include "stat.h"
 #include "user.h"
 #include "fs.h"
+#include "fcntl.h"
+#include "syscall.h"
+#include "traps.h"
+#include "memlayout.h"
 
-int main(int argc, char *argv[]){
-  int pid;
-  int wtime, rtime;
-  pid = fork();
-  if(pid == 0)
-  {
-    exec(argv[1],argv);
-    printf(1,"exec %s failed\n",argv[1]);
-  }
-  
-//   else{
-    waitx(&wtime, &rtime);
-//     printf(1,"Status: %d\n",status);
-//     } 
-    
-    printf(1,"Wait Time = %d\n",wtime);
-    printf(1,"Run Time = %d\n",rtime);
-//     printf(1,"Status: %d\n",status);
+int main(int argc, char *argv[])
+{
+    int wtime;
+    int rtime;
+
+    printf(1, "Testing waitx()...\n");
+
+    int pid = fork();
+    if(pid == 0) //child process
+    {
+      exec(argv[1], argv + 1);
+      exit();      
+    }
+
+    waitx(&wtime,&rtime);
+
+    printf(1, "--> Waiting time: %d\n", wtime);
+    printf(1, "--> Running time: %d\n", rtime);
+
     exit();
-    
+
+    return 0;
 }
