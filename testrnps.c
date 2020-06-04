a79c378c5194abc763e2faaea429ee2d9b313873
Mobin Dariush <mobindariush@outlook.com>
2020-06-05 02:59:44 +0430

    Add fork sys calls for testing get_process_info

--- testrnps.c
+++ testrnps.c
@@ -5,11 +5,26 @@
 
 int main(void)
 {
-  //struct proc_info *p = malloc(100 * sizeof(struct proc_info));
-  //struct proc_info **p_infos = &p;
+  struct proc_info p[100];
   
-  struct proc_info *p;
+  int pid = fork();
 
-  rnps(&p);
+  if (pid > 0)
+  {
+    wait();
+    printf(0, "Continuing parent");
+  } else if (pid == 0)
+  {
+    printf(0, "In Child now");
+    rnps(p);
+    for (int i = 0; i < 100; i++)
+    {
+      if (p[i].pid == 0) break; //A zero pid indicates the end of process list
+      printf(0, "\n\nprocess => pid : %d, name : %s\n\n", p[i].pid, p[i].name);
+    }
+  } else
+  {
+    printf(0, "Error forking");
+  }
   exit();
 }
