d2240fa214ea64ce7df19d7ed22b9fb73d9a9f44
Mobin Dariush <mobindariush@outlook.com>
2020-06-04 21:00:57 +0430

    Add process info struct

--- proc.h
+++ proc.h
@@ -51,6 +51,10 @@ struct proc {
   char name[16];               // Process name (debugging)
 };
 
+struct proc_info {
+  int pid;
+  int memsize; // in bytes
+};
 // Process memory is laid out contiguously, low addresses first:
 //   text
 //   original data and bss
