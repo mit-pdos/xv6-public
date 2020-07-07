376b35647b16f790bff01185b6e24cc6e334ad4b
Mobin Dariush <mobindariush@outlook.com>
2020-07-07 12:54:00 +0430

    Add new fields to proc struct

--- proc.h
+++ proc.h
@@ -49,6 +49,10 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int stime;                   // Start time
+  int etime;                   // End time
+  int iotime;                  // IO time
+  int rtime;                   // Remaining time
 };
 // Process memory is laid out contiguously, low addresses first:
 //   text
