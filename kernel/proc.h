d6dad42aaffe7460025f103ed3807f589df6b24d
Robert Morris <rtm@csail.mit.edu>
2020-07-17 16:29:52 -0400

    rename p->tf to p->trapframe, for consistency with p->context

--- kernel/proc.h
+++ kernel/proc.h
@@ -98,7 +98,7 @@ struct proc {
   uint64 kstack;               // Virtual address of kernel stack
   uint64 sz;                   // Size of process memory (bytes)
   pagetable_t pagetable;       // Page table
-  struct trapframe *tf;        // data page for trampoline.S
+  struct trapframe *trapframe; // data page for trampoline.S
   struct context context;      // swtch() here to run process
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
