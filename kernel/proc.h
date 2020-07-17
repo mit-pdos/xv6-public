3b053f5d58b4914c6389588ad4e556bc887bc99d
Robert Morris <rtm@csail.mit.edu>
2020-07-17 16:40:57 -0400

    cpu->scheduler -> cpu->context to reduce confusion

--- kernel/proc.h
+++ kernel/proc.h
@@ -21,7 +21,7 @@ struct context {
 // Per-CPU state.
 struct cpu {
   struct proc *proc;          // The process running on this cpu, or null.
-  struct context scheduler;   // swtch() here to enter scheduler().
+  struct context context;     // swtch() here to enter scheduler().
   int noff;                   // Depth of push_off() nesting.
   int intena;                 // Were interrupts enabled before push_off()?
 };
