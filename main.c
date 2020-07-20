1a944f6a8d25e605eeb19d5ba984417a6bcd925a
Sergei Rogachev <rogachevsergei@gmail.com>
2020-07-20 17:59:14 +0300

    Fix tripple fault when paging is enabled
    entrypgdir is not initialized properly (filled
    with zeroes, accidentally). Enabling of paging
    causes a page fault immediately, this results
    to a tripple fault.
    
    Looks like the problem can be observed with
    new versions of the toolchain.
    
    Reproduced by me and my colleague.
    
    GCC version:
    gcc version 9.3.0 (Ubuntu 9.3.0-10ubuntu2)

--- main.c
+++ main.c
@@ -57,7 +57,18 @@ mpmain(void)
   scheduler();     // start running processes
 }
 
-pde_t entrypgdir[];  // For entry.S
+// The boot page table used in entry.S and entryother.S.
+// Page directories (and page tables) must start on page boundaries,
+// hence the __aligned__ attribute.
+// PTE_PS in a page directory entry enables 4Mbyte pages.
+
+__attribute__((__aligned__(PGSIZE)))
+const pde_t entrypgdir[NPDENTRIES] = {
+  // Map VA's [0, 4MB) to PA's [0, 4MB)
+  [0] = (0) | PTE_P | PTE_W | PTE_PS,
+  // Map VA's [KERNBASE, KERNBASE+4MB) to PA's [0, 4MB)
+  [KERNBASE>>PDXSHIFT] = (0) | PTE_P | PTE_W | PTE_PS,
+};
 
 // Start the non-boot (AP) processors.
 static void
@@ -93,19 +104,6 @@ startothers(void)
       ;
   }
 }
-
-// The boot page table used in entry.S and entryother.S.
-// Page directories (and page tables) must start on page boundaries,
-// hence the __aligned__ attribute.
-// PTE_PS in a page directory entry enables 4Mbyte pages.
-
-__attribute__((__aligned__(PGSIZE)))
-pde_t entrypgdir[NPDENTRIES] = {
-  // Map VA's [0, 4MB) to PA's [0, 4MB)
-  [0] = (0) | PTE_P | PTE_W | PTE_PS,
-  // Map VA's [KERNBASE, KERNBASE+4MB) to PA's [0, 4MB)
-  [KERNBASE>>PDXSHIFT] = (0) | PTE_P | PTE_W | PTE_PS,
-};
 
 //PAGEBREAK!
 // Blank page.
