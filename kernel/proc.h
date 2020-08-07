7f35d7a14efe56905d340a0e318f01c7c514320d
Robert Morris <rtm@csail.mit.edu>
2020-08-07 05:32:48 -0400

    modify each page in usertests countfree() get rid of static for walk() and freewalk()

--- kernel/proc.h
+++ kernel/proc.h
@@ -97,7 +97,7 @@ struct proc {
   // these are private to the process, so p->lock need not be held.
   uint64 kstack;               // Virtual address of kernel stack
   uint64 sz;                   // Size of process memory (bytes)
-  pagetable_t pagetable;       // Page table
+  pagetable_t pagetable;       // User page table
   struct trapframe *trapframe; // data page for trampoline.S
   struct context context;      // swtch() here to run process
   struct file *ofile[NOFILE];  // Open files
