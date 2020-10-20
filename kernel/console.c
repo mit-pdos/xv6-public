13dccb0380d3c694083095040fadd03bfe4f598c
Robert Morris <rtm@csail.mit.edu>
2020-10-20 06:55:51 -0400

    consolewrite does not need cons.lock -- can lead to sleep() with lock held

--- kernel/console.c
+++ kernel/console.c
@@ -60,14 +60,12 @@ consolewrite(int user_src, uint64 src, int n)
 {
   int i;
 
-  acquire(&cons.lock);
   for(i = 0; i < n; i++){
     char c;
     if(either_copyin(&c, user_src, src+i, 1) == -1)
       break;
     uartputc(c);
   }
-  release(&cons.lock);
 
   return i;
 }
