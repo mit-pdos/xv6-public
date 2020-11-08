e07ca6614599e68747fa454da95f007f6941143a
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2020-11-08 01:56:57 -0500

    comment

--- kernel/proc.h
+++ kernel/proc.h
@@ -93,7 +93,7 @@ struct proc {
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
 
-  // proc_tree_lock must be held when using this:
+  // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
 
   // these are private to the process, so p->lock need not be held.
