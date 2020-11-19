6e3f75c2aa670bc0d47928cd48408b81e5bd758d
Robert Morris <rtm@csail.mit.edu>
2020-11-19 16:48:41 -0500

    suppress an incorrect error message in grind

--- user/grind.c
+++ user/grind.c
@@ -277,14 +277,15 @@ go(int which_child)
       close(aa[0]);
       close(aa[1]);
       close(bb[1]);
-      char buf[3] = { 0, 0, 0 };
+      char buf[4] = { 0, 0, 0, 0 };
       read(bb[0], buf+0, 1);
       read(bb[0], buf+1, 1);
+      read(bb[0], buf+2, 1);
       close(bb[0]);
       int st1, st2;
       wait(&st1);
       wait(&st2);
-      if(st1 != 0 || st2 != 0 || strcmp(buf, "hi") != 0){
+      if(st1 != 0 || st2 != 0 || strcmp(buf, "hi\n") != 0){
         printf("grind: exec pipeline failed %d %d \"%s\"\n", st1, st2, buf);
         exit(1);
       }
