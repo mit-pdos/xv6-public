0dbdd692392aaa40e80f3ac36744acfdb53b9ac9
ahmadyasser01 <47822365+ahmadyasser01@users.noreply.github.com>
2021-01-10 19:47:13 +0200

    nullpointer derefence (#9)

--- exec.c
+++ exec.c
@@ -39,7 +39,7 @@ exec(char *path, char **argv)
     goto bad;
 
   // Load program into memory.
-  sz = 0;
+  sz = PGSIZE;
   for(i=0, off=elf.phoff; i<elf.phnum; i++, off+=sizeof(ph)){
     if(readi(ip, (char*)&ph, off, sizeof(ph)) != sizeof(ph))
       goto bad;
