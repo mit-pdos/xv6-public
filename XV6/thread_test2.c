e467f7ead4bf5ed149dbc370c3266386b0989730
shahendahamdy <shahendahamdy666@gmail.com>
2021-01-27 05:40:33 +0200

    Test2

--- XV6/thread_test2.c
+++ XV6/thread_test2.c
@@ -3,91 +3,57 @@
 #include "user.h"
 #include "fcntl.h"
 #define NULL (void *)(0)
-//summation thread
-void s(void* v1,void* v2){
-printf(1,"1st thread'c child2 \n");
-printf(1,"%d + %d is : %d \n",*(int*)v1, *(int*)v2,*(int*)v1 + *(int*)v2 );
+struct lock_t *L ;
 
-exit();
+int cnt = 0 ; //global variable must be put in critical region
+
+void f1(){
+   for(int i=0;i<10;i++){
+	int pid =getpid();
+	//critical region
+	lock_acquire(L);
+	printf(1,"PID : %d -->cnt: %d \n",pid,cnt++);
+	lock_release(L);
+   }
+   exit();
 }
-//power thread >> and calls thread that do the summation
-void p(void* argm1,void* argm2){
-printf(1,"1st thread'c child1 \n");
-printf(1,"%d power %d is ",*(int*)argm1,*(int*)argm2);
-int i , res =1;
-for(i=0; i< *(int*) argm2; i++){
- res = (*(int*) argm1 ) * res;
-}
-printf(1, ": %d\n",res);
-thread_create(&s,argm1,argm2);
-thread_join();
-exit();
-}
-//factorial thread
-void f(void* arg1,void* arg2){
-printf(1,"1st thread \n");
-printf(1,"%d factorial is ",*(int*)arg1);
-int i , res =1;
-for(i=1; i<= *(int*) arg1; i++){
- res = res *i;
-}
-printf(1, ": %d\n",res);
-  thread_create(&p,arg1,arg2);
-  thread_join();
-exit();
+//without applying the locks , a race condition may happen.
+void f2(){
+   for(int i=0;i<10;i++){
+	int pid =getpid();
+	
+	printf(1,"PID : %d -->cnt: %d \n",pid,cnt++);
+   }
+   exit();
 }
 
 
 int
 main(int argc, char *argv[])
 {
-  
-  int x=3,y=4;
-  printf(1, "RUNNING..\n");
-  thread_create(&f,&x,&y);
-  thread_join();
-  printf(1, "FINISHING .\n");
-  exit();
+  L = malloc(16);
 
-}
-
-
-/*#include "types.h"
-#include "stat.h"
-#include "user.h"
-#include "fcntl.h"
-#define NULL (void *)(0)
-struct lock_t *L;
-int cnt = 0 ;
-void funct(){
-for(int i =0 ; i<10000; i++)cnt++;
-exit();
-}
-
-int
-main(int argc, char *argv[])
-{
- 
   printf(1, "RUNNING..\n");
   lock_init(L);
-  lock_acquire(L);
-  thread_create(&funct, NULL,NULL);
-  lock_release(L);
+  thread_create(&f1, NULL,NULL);
+  thread_create(&f1, NULL,NULL);
+  thread_create(&f1, NULL,NULL);
   thread_join();
-  printf(1,"1st increment %d\n",cnt);
-  //----
-  lock_acquire(L);
-  thread_create(&funct, NULL,NULL);
-  lock_release(L);
   thread_join();
-  printf(1,"2nd increment %d\n",cnt);
-  //-----
-  lock_acquire(L);
-  thread_create(&funct, NULL,NULL);
-  lock_release(L);
   thread_join();
-  printf(1,"3rd increment %d\n",cnt);
+  
+  /*
+  thread_create(&f2, NULL,NULL);
+  thread_create(&f2, NULL,NULL);
+  thread_create(&f2, NULL,NULL);
+  thread_join();
+  thread_join();
+  thread_join();
+ */
+  printf(1,"\n The Total Count :: %d\n",cnt);
   printf(1, "EXITING  \n");
+  
   exit();
 
-}*/
+}
+
