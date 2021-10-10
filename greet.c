0d3cd2e43b161819dfb5f48581741fa7830e9ce7
Francisco Maldonado <fmald002@sledge.cs.ucr.edu>
2021-10-09 17:36:11 -0700

    Lab 0.5, Add greet system call

---
+++ greet.c
@@ -0,0 +1,8 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main (int agrc, char * argv[]) {
+    greeting();
+    exit();
+}
--- sysproc.c
+++ sysproc.c
@@ -89,3 +89,9 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+int
+sys_greeting(void) {
+  greeting();
+  return 0;
+}
