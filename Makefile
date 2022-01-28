e3c7252b23a9d6212ea6110167ea46049eb9a160
Abhay Koushal <abhaykoushal2000@gmail.com>
2022-01-28 15:18:56 +0530

    application added : yes

--- Makefile
+++ Makefile
@@ -181,6 +181,7 @@ UPROGS=\
 	_usertests\
 	_wc\
 	_zombie\
+	_yes\
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
---
+++ yes.c
@@ -0,0 +1,12 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+  for (int i = 0; i < 100; i++)
+    	printf(1, "%s\n", (argc>1? argv[1]: "y"));
+      // printf(1, "y\n");
+  exit();
+}
