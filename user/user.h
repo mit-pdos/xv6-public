f2ee8690addf0aa52b5de2061fe6d659574406f9
Frans Kaashoek <kaashoek@mit.edu>
2022-08-17 20:38:11 -0400

    x

--- user/user.h
+++ user/user.h
@@ -9,7 +9,7 @@ int write(int, const void*, int);
 int read(int, void*, int);
 int close(int);
 int kill(int);
-int exec(char*, char**);
+int exec(const char*, char**);
 int open(const char*, int);
 int mknod(const char*, short, short);
 int unlink(const char*);
