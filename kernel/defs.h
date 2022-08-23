ac8c0bb177da1119364d10a1a25a14b6dd238cce
Frans Kaashoek <kaashoek@mit.edu>
2022-08-23 11:56:29 -0400

    x

--- kernel/defs.h
+++ kernel/defs.h
@@ -136,7 +136,7 @@ char*           strncpy(char*, const char*, int);
 // syscall.c
 void            argint(int, int*);
 int             argstr(int, char*, int);
-void             argaddr(int, uint64 *);
+void            argaddr(int, uint64 *);
 int             fetchstr(uint64, char*, int);
 int             fetchaddr(uint64, uint64*);
 void            syscall();
