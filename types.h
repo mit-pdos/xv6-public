05f39890685fca4aed016f98a8beefb6313ec5c7
Farzan <74285751+FarzanRahmani@users.noreply.github.com>
2023-01-31 00:19:26 +0330

    types

--- types.h
+++ types.h
@@ -2,3 +2,10 @@ typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
 typedef uint pde_t;
+
+// Project Struct as said in the project description
+struct proc_info
+{
+  int pid;
+  int memsize;
+};
