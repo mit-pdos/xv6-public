e485e5ed48d58f22d872ce75671bc0a2461a5f8d
Erik M. Buck <20407477+Make-IT-Wright@users.noreply.github.com>
2023-03-20 08:27:08 -0400

    Added ps program make system call and output information about current processes

--- ps.c
+++ ps.c
@@ -2,16 +2,25 @@
 #include "stat.h"
 #include "user.h"
 
-struct procInfo arrayOfProcInfo[1024];
+static struct procInfo arrayOfProcInfo[1024];
+static const char * const states[] = {
+  [UNUSED]    "unused",
+  [EMBRYO]    "embryo",
+  [SLEEPING]  "sleep ",
+  [RUNNABLE]  "ready ",
+  [RUNNING]   "run   ",
+  [ZOMBIE]    "zombie"
+  };
 
 int
 main(int argc, char *argv[])
 {
-
   int result = ps(1024, arrayOfProcInfo);
   for(int i = 0; i < result; ++i) {
-    printf(1, "%s\n", arrayOfProcInfo[i].name);
+    printf(1, "%d: %s %d %s\n", arrayOfProcInfo[i].pid,
+      states[arrayOfProcInfo[i].state], 
+      arrayOfProcInfo[i].cpuPercent,
+      arrayOfProcInfo[i].name);
   }
-  printf(1, "ps returned <%d>!\n", result);
   exit();
 }
