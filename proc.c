a98157a5ce1c8cb4d73d8f543834898aed3779ef
EricCordts <ecordts@DESKTOP-5H7V7B5.localdomain>
2023-03-20 19:58:08 -0400

    Working MLFQ scheduler

--- proc.c
+++ proc.c
@@ -27,6 +27,7 @@ extern void trapret(void);
 static void wakeup1(void *chan);
 
 void removeProcessFromPriorityQueue(int priority, int indexInQueue);
+int findIndexInPriorityQueue(int priority);
 
 void
 pinit(void)
@@ -117,6 +118,11 @@ allocproc(void)
 found:
   p->state = EMBRYO;
   p->pid = nextpid++;
+  // Edited by Eric Cordts and Jonathan Hsin
+  p->priority = 1;
+  p->indexInQueue = findIndexInPriorityQueue(p->priority);
+  ptable.priorityQueue[1][p->indexInQueue] = p;
+  ptable.queueCount[1]++;
 
   release(&ptable.lock);
 
@@ -195,18 +201,7 @@ userinit(void)
   // writes to be visible, and the lock is also needed
   // because the assignment might not be atomic.
   acquire(&ptable.lock);
-
-  // Edited by Jonathan Hsin and Eric Cordts
-  // Assign default priority of user process 
-  // to 1 and put it into appropriate queue
-  p->priority = 1;
-  p->indexInQueue = findIndexInPriorityQueue(p->priority);
-  cprintf("Priority: %d\n", p->priority);
-  cprintf("indexInQueue: %d\n", p->indexInQueue);
-  ptable.priorityQueue[p->priority][p->indexInQueue] = p;
-  ptable.queueCount[p->priority]++;
   p->state = RUNNABLE;
-
   release(&ptable.lock);
 }
 
@@ -270,18 +265,7 @@ fork(void)
   pid = np->pid;
 
   acquire(&ptable.lock);
-
-  // Edited by Jonathan Hsin and Eric Cordt
-  // new process set to priority 1 and added to 
-  // appropriate queue
-  np->priority = 1;
-  np->indexInQueue = findIndexInPriorityQueue(np->priority);
-  cprintf("in Fork, priority: %d, indexinQueue: %d, pid: %d\n", np->priority, np->indexInQueue, np->pid);
-  ptable.priorityQueue[np->priority][np->indexInQueue] = np;
-  ptable.queueCount[np->priority]++;
-
   np->state = RUNNABLE;
-
   release(&ptable.lock);
 
   return pid;
@@ -407,11 +391,9 @@ scheduler(void)
     
     // First check the top priority queue and run it RR
     // until it is empty (queueCount == 0)
-    
-    //cprintf("here\n");
     while(ptable.queueCount[0] > 0)
     {
-	//cprintf("here1\n");
+	int runnableInQueue0 = 0;
 	int i;
 	for(i = 0; i < NPROC; i++)
 	{
@@ -420,10 +402,10 @@ scheduler(void)
 	    {
 		continue;
 	    }
-	    //cprintf("here11");
 	    // Switch to chosen process. It is the process' 
 	    // job to release ptable.lock and then reacquire it 
 	    // before jumping back to us.
+	    runnableInQueue0 = 1;
 	    c->proc = p;
 	    switchuvm(p);
 	    p->state = RUNNING;
@@ -435,25 +417,26 @@ scheduler(void)
 	    // It should have changed its p->state before coming back.
 	    c->proc = 0;
 	}
+	if(runnableInQueue0 == 0)
+	{
+	    break;
+	}
     }
-    //cprintf("here2\n");
     // Then check the middle priority queue 
     // and run it RR until it is empty OR until 
     // the queueCount of the top priority queue is no longer 0.
     while(ptable.queueCount[0] == 0 && ptable.queueCount[1] > 0)
     {
-	//cprintf("here3\n");
+	int runnableInQueue1 = 0;
 	int j;
-	//cprintf("count of queue: %d\n", ptable.queueCount[1]);
 	for(j = 0; j < NPROC; j++)
 	{
-	    //cprintf("Value of j: %d\n", j);
 	    p = ptable.priorityQueue[1][j];
 	    if(p == NULL || p->state != RUNNABLE)
 	    {
 		continue;
 	    }
-	    //cprintf("here33");
+	    runnableInQueue1 = 1;
 	    // Switch to chosen process. It is the process' job
 	    // to release ptable.lock and then reacquire it before
 	    // jumping back to us
@@ -463,7 +446,6 @@ scheduler(void)
 
 	    swtch(&c->scheduler, p->context);
 	    switchkvm();
-	    //cprintf("Here333");
 	    // process is done running for now
 	    // It should have changed its p->state before coming back.
 	    c->proc = 0;
@@ -475,16 +457,18 @@ scheduler(void)
 		break;
 	    }
 	}
+	if(runnableInQueue1 == 0)
+	{
+	    break;
+	}
     }
 
-    //cprintf("here4\n");
     // Now check the last priority queue and run it RR 
     // until it is empty OR until the queueCount of 
     // either the top or middle queue is no longer 0.
     while(ptable.queueCount[0] == 0 && ptable.queueCount[1] == 0 && ptable.queueCount[2] > 0)
     {
-	//cprintf("here5\n");
-	//sleep(1);
+	int runnableInQueue2 = 0;
 	int k;
 	for(k = 0; k < NPROC; k++)
 	{
@@ -493,8 +477,7 @@ scheduler(void)
 	    {
 		continue;
 	    }
-	    //cprintf("here6\n");
-	    //sleep(1);
+	    runnableInQueue2 = 1;
 	    c->proc = p;
 	    switchuvm(p);
 	    p->state = RUNNING;
@@ -509,10 +492,13 @@ scheduler(void)
 		break;
 	   } 
 	}
+	if(runnableInQueue2 == 0)
+	{
+	   break;
+	}
     }
-
-
     /*
+    COMMENTED OUT RR Scheduler
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
       if(p->state != RUNNABLE)
         continue;
