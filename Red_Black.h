1cc1a71672f11993e697a1ec024ca3181e5c896a
Ali <aalimomen110@gmail.com>
2023-11-26 14:28:38 +0330

    Basics of Red_Black tree

---
+++ Red_Black.h
@@ -0,0 +1,20 @@
+#include"proc.h"
+#include "defs.h"
+//#define Null ((void*)0)
+enum Color {Red,Black};
+void Print(struct proc *p){
+    cprintf("The name is %s\n",p->name);
+}
+struct node{
+    struct node *left,*right,*parent;
+    struct proc *Process;
+    enum Color node_color;
+};
+/*struct node *Create_Red_Black_Tree(struct proc* p){
+    struct node *New=(struct node *)malloc(sizeof(struct node));
+    New->node_color=Red;
+    New->Process=p;
+    New->left=Null;
+    New->right=Null;
+    New->parent=Null;
+}*/
--- proc.c
+++ proc.c
@@ -1,12 +1,12 @@
 #include "types.h"
-#include "defs.h"
+
 #include "param.h"
 #include "memlayout.h"
 #include "mmu.h"
 #include "x86.h"
-#include "proc.h"
-#include "spinlock.h"
 
+#include "spinlock.h"
+#include"Red_Black.h"
 struct {
   struct spinlock lock;
   struct proc proc[NPROC];
