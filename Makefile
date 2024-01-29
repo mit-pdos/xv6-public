765e505043369fe4069577ddf16eda11edb50337
Keval <kevalkataria@gmail.com>
2024-01-29 18:36:50 -0500

    Initial Commit

--- Makefile
+++ Makefile
@@ -181,6 +181,7 @@ UPROGS=\
 	_usertests\
 	_wc\
 	_zombie\
+	_sleep\
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
---
+++ sleep.c
@@ -0,0 +1,20 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
+
+int
+main(int argc, char *argv[])
+{
+    int ticks;
+    if(argc != 2) {
+        printf(1, "%s\n", "Error: sleep requires exactly 1 argument");
+        exit();
+    }
+    ticks = atoi(argv[1]);
+    if(ticks <= 0) {
+        printf(1, "%s\n", "Error: sleep ticks must be a positive value");
+        exit();
+    }
+    sleep(ticks);
+}
