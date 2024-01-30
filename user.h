d6b04e1ffedd61c31ded863bbcdc388a14d62137
Mobin Barfi <m_barfi@comp.iust.ac.ir>
2024-01-30 07:31:42 +0330

    lock system and lock functions defined

--- user.h
+++ user.h
@@ -42,3 +42,14 @@ int atoi(const char*);
 //Thread library
 int thread_create(void (*)(void*,void*),void*,void*);
 int thread_join(int thread_id);
+
+//Lock System
+typedef struct Lock
+{
+    uint Is_Locked;
+} Lock;
+
+//Lock Functions
+void Lock_Init(Lock* mutex);
+void Lock_Acquire(Lock* mutex);
+void Lock_Release(Lock* mutex);
